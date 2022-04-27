485bce49e09f1bcf78d1d43dac5143f680e1e9d7
BaviAndz <baviandz97@gmail.com>
2022-04-27 22:18:55 +0530

    new demo local

---
+++ src/main/java/com/example/demogitproj/DemogitprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogitproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demogitproj/DemogitprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogitproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
